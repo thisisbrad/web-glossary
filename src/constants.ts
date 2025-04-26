cd1a230128b7272e48dd2de13b03db65d8a4d7e5
Brad Beltowski <bradbeltowskigmail.com>
2025-04-26 15:55:31 -0400

    Adding single pages

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Tech Talk";
+export const SITE_DESCRIPTION = "Articles, Storeis and Tutorials for Tech Talk";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
