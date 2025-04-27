e3dff2618740590884364152d760562c37e13220
Brad Beltowski <bradbeltowskigmail.com>
2025-04-26 23:41:28 -0400

    Homepage is now dynamic like Articles page.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Tech Talk";
 export const SITE_DESCRIPTION = "Articles, Storeis and Tutorials for Tech Talk";
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
