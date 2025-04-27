8894d96fced0f06d232f0c3aa455cc642e78d8a9
Brad Beltowski <bradbeltowskigmail.com>
2025-04-26 22:59:28 -0400

    Sorting Articles by date.

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,16 @@
+export default function formatDate(date: Date, format: string): string {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return format
+    .replace("YYYY", year.toString())
+    .replace("MM", month)
+    .replace("DD", day)
+    .replace("HH", hours)
+    .replace("mm", minutes)
+    .replace("ss", seconds);
+}
